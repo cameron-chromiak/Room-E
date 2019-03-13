3aa4bfd2709deb82f1d27407672552a7752071e5
cameron-chromiak <cam.chromiak@hotmail.com>
2019-03-13 10:55:21 -0400

    -worked on Dashboard

--- client/src/components/pages/Dashboard.jsx
+++ client/src/components/pages/Dashboard.jsx
@@ -19,16 +19,30 @@ export default class Dashoard extends Component {
     if(this.props.stuff.isLoggedIn){
       return (
         <div>
-          <h2>Dashoard</h2>
-
+        <h2>Dashoard</h2>
+        <div class="ui four column doubling stackable grid container">
+  <div class="column">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos sequi omnis et sunt corrupti non, dignissimos labore sapiente illum, veritatis, laborum amet aliquid atque vel.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos sequi omnis et sunt corrupti non, dignissimos labore sapiente illum, veritatis, laborum amet aliquid atque vel.</p>
+  </div>
+</div>
+        <div className="ui raised very padded text container segment">
+        <h3>Create a new Home</h3>
+          <form class="ui form">
+            <div class="field">
+              <label>First Name</label>
+              <input type="text" name="first-name" placeholder="First Name"/>
+            </div>
+          </form>
+        </div>
         </div>
       )
     }
     else {
       return (
         <div>
-          login
-          {/*<Redirect to="/"/>*/}
+        <Redirect to="/"/>
+          {/*{setTimeout(<Redirect to="/"/>, 1000)}*/}
         </div>
       )
     }
