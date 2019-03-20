5f6389cdaab014a50335220564c097b55e723c52
cameron-chromiak <cam.chromiak@hotmail.com>
2019-03-20 17:38:05 -0400

    commented

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,30 +1,30 @@
-const express = require('express');
-const { isLoggedIn } = require('../middlewares')
-const router = express.Router();
-const House = require('../models/House')
-
-// router.delete('/random/:deleteId', (req, res, next)=>{
-//   console.log('DELETE RANDOM', req.params)
-//   Country.remove({_id:req.params.deleteId}).then(deletedThing=>{
-//     console.log('deleted', deletedThing)
-//     res.json({deleted:deletedThing})
+// const express = require('express');
+// const { isLoggedIn } = require('../middlewares')
+// const router = express.Router();
+// const House = require('../models/House')
+//
+// // router.delete('/random/:deleteId', (req, res, next)=>{
+// //   console.log('DELETE RANDOM', req.params)
+// //   Country.remove({_id:req.params.deleteId}).then(deletedThing=>{
+// //     console.log('deleted', deletedThing)
+// //     res.json({deleted:deletedThing})
+// //   })
+// // })
+// router.get('/dashboard', isLoggedIn, (req, res, next) =>{
+//   console.log('INDEX')
+//   let houses = House.find({userId:req.user._id}).then(house =>{
+//     res.json({
+//       houses:house
+//     })
 //   })
 // })
-router.get('/dashboard', isLoggedIn, (req, res, next) =>{
-  console.log('INDEX')
-  let houses = House.find({userId:req.user._id}).then(house =>{
-    res.json({
-      houses:house
-    })
-  })
-})
-
-
-router.get('/secret', isLoggedIn, (req, res, next) => {
-  res.json({
-    secret: 42,
-    user: req.user
-  });
-});
-
-module.exports = router;
+//
+//
+// router.get('/secret', isLoggedIn, (req, res, next) => {
+//   res.json({
+//     secret: 42,
+//     user: req.user
+//   });
+// });
+//
+// module.exports = router;
