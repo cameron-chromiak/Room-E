a77431e360bbe22f82b1929889afd1dd36dcaa57
cameron-chromiak <cam.chromiak@hotmail.com>
2019-03-22 10:08:59 -0400

    gitign

---
+++ client/build/precache-manifest.3a9c976620dfc8cccf199c7688ea6567.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d382ca63d14c5cd7c31",
+    "url": "/static/js/main.4c891905.chunk.js"
+  },
+  {
+    "revision": "68e955e9762030b7f0f0",
+    "url": "/static/js/2.c3156c59.chunk.js"
+  },
+  {
+    "revision": "5d382ca63d14c5cd7c31",
+    "url": "/static/css/main.8126b383.chunk.css"
+  },
+  {
+    "revision": "475f272ffba8cd36d9db8678e14947f3",
+    "url": "/index.html"
+  }
+];
